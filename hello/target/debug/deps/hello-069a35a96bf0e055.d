03ccb8a469231c0c9e8f46262092b0dce05f1920
emil <emil.laudn@gmail.com>
2022-04-01 22:17:25 +0200

    initial commit

---
+++ hello/target/debug/deps/hello-069a35a96bf0e055.d
@@ -0,0 +1,5 @@
+c:\Users\emill\projects\challenges\hello\target\debug\deps\hello-069a35a96bf0e055.rmeta: src\main.rs
+
+c:\Users\emill\projects\challenges\hello\target\debug\deps\hello-069a35a96bf0e055.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/deps/hello-c58a6b78f21c2088.d
@@ -0,0 +1,5 @@
+c:\Users\emill\projects\challenges\hello\target\debug\deps\hello-c58a6b78f21c2088.rmeta: src\main.rs
+
+c:\Users\emill\projects\challenges\hello\target\debug\deps\hello-c58a6b78f21c2088.d: src\main.rs
+
+src\main.rs:
